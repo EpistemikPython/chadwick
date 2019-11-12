ecf520d34f4a1fbb2c3ec9574f7cb0b4d5721ffe
epistemik <epistemik@gmail.com>
2019-11-11 20:34:53 -0500

    more progress on implementing cwtools functions

--- try_chadwick2.py
+++ try_chadwick2.py
@@ -1,4 +1,6 @@
-import sys
+import ctypes
+import itertools
+
 from chadwick.libchadwick import *
 
 
@@ -18,52 +20,35 @@ def cwbox_print_header(p_game, p_vis, p_home):
     year, month, day = cw_game_info_lookup(p_game, "date").split('/')
     game_number = cw_game_info_lookup(p_game, "number")
     game_number_str = "" if game_number == "0" else ", game %s".format(game_number)
-    print("     Game of %s/%s/%s%s -- %s at %s (%s)\n" % ( month, day, year, game_number_str,
+    print("     Game of %s/%s/%s%s -- %s at %s (%s)" % ( month, day, year, game_number_str,
                 p_vis.city if p_vis else cw_game_info_lookup(p_game, "visteam"),
                 p_home.city if p_home else cw_game_info_lookup(p_game, "hometeam"),
                 dn_code ))
-    print("\n")
+    print("")
 
 
 # void cwbox_print_player(CWBoxPlayer *player, CWRoster *roster)
 def cwbox_print_player(p_player, p_roster):
-    # print("type(p_player) = {}".format(type(p_player)))
-    # print("type(p_roster) = {}".format(type(p_roster)))
     # print( "%s %s" % (str(p_player),str(p_team)) )
     bio = None # CWPlayer *
     name = p_player.name
     if p_roster:
         bio = cw_roster_player_find(p_roster, p_player.player_id)
-        # print("found roster")
     if bio:
         name = bio.last_name + bio.first_name[0]
-        # print("found bio")
-    # print("name = {}".format(name))
 
     posn_str = ""
-    # print("p_player.positions = {}".format(repr(p_player.positions)))
-    # print("type(p_player.positions) = {}".format(type(p_player.positions)))
-    # print("p_player._get_position(0) = {}".format(p_player._get_position(0)))
-    # print("p_player.positions[0] = {}".format(p_player.positions[0]))
     if p_player.ph_inn > 0 and p_player._get_position(0) != 11:
         posn_str = "ph"
     elif p_player.pr_inn > 0 and p_player._get_position(0) != 12:
         posn_str = "pr"
-    # print("posn_str = {}".format(posn_str))
 
-    # print("positions = {}".format(positions))
-    # print("positions[5] = {}".format(positions[5]))
-    # print("p_player.num_positions = {}".format(p_player.num_positions))
     for pos in range(0, p_player.num_positions):
-        # print("pos = {}".format(str(pos)))
         if len(posn_str) > 0:
             posn_str += "-"
-            # print("new posn_str = {}".format(posn_str))
         posn_str += positions[p_player._get_position(pos)]
-    # print("new posn_str = {}".format(posn_str))
 
     posn_str_len = len(posn_str)
-    # print("posn_str_len = {}".format(posn_str_len))
     if len(posn_str) <= 10:
         if len(posn_str) + len(name) > 18:
             out_str = name[0:(18 - posn_str_len)] + ", "
@@ -73,7 +58,6 @@ def cwbox_print_player(p_player, p_roster):
     else:
         # When there are a lot of positions, can't do much sensibly...
         out_str = name + ", " + positions[p_player._get_position(0)] + ",..."
-    # print("out_str = {}".format(out_str))
 
     if p_player.batting.bi != -1:
         print("%-20s %2d %2d %2d %2d" % (out_str, p_player.batting.ab, p_player.batting.r, p_player.batting.h, p_player.batting.bi)),
@@ -81,9 +65,75 @@ def cwbox_print_player(p_player, p_roster):
         print("%-20s %2d %2d %2d   " % (out_str, p_player.batting.ab, p_player.batting.r, p_player.batting.h)),
 
 
-# cwbox_print_linescore(game, boxscore, visitors, home)
+# void cwbox_print_linescore(CWGame *game, CWBoxscore *boxscore, CWRoster *visitors, CWRoster *home)
 def cwbox_print_linescore(p_game, p_box, p_vis, p_home):
-    print( "%s %s %s %s" % (str(p_game),str(p_box),str(p_vis),str(p_home)) )
+    # print( "%s %s %s %s" % (str(p_game),str(p_box),str(p_vis),str(p_home)) )
+    i = 0
+    for t in range(0,2):
+        runs = 0
+        if t == 0:
+            print("%-17s" % p_vis.city if p_vis else cw_game_info_lookup(p_game, "visteam"))
+        else:
+            print("%-17s" % p_home.city if p_home else cw_game_info_lookup(p_game, "hometeam"))
+        for i in range(1,50):
+            linescore = p_box.linescore
+            print("linescore = {}".format(str(linescore)))
+
+            def swig_to_str(s, l):
+                base = int(s)
+                ty = ctypes.c_ubyte * 1
+
+                # for x in itertools.count():
+                v = ty.from_address(base + i)[0]
+                if not v: return
+                l.append(v)
+                return v
+
+            score_list = list()
+            swig_to_str(linescore, score_list)
+            print("score_list = {}".format(score_list))
+
+            def swig_to_byte(b):
+                ty = ctypes.c_ubyte*1
+                print("ty = %s" % ty)
+                b_int = int(b)
+                print("b_int = 0x%x" % b_int)
+                v = ty.from_address(b_int)
+                print("v = {}".format(v))
+                # v2 = ty.from_address(v)
+                return v
+
+            byte_score = swig_to_byte(linescore)
+            print("byte_score = {}".format(str(byte_score)))
+            print("byte_score[0] = {}".format(str(byte_score[0])))
+            # print("byte_score[1] = {}".format(str(byte_score[1])))
+
+            score = linescore[i]
+            print("score = {}".format(score))
+            if score[0] < 0 and p_box.linescore[i][1] < 0:
+                break
+            if p_box.linescore[i][t] >= 10:
+                print("(%d)" % p_box.linescore[i][t])
+                runs += p_box.linescore[i][t]
+            elif p_box.linescore[i][t] >= 0:
+                print("%d" % p_box.linescore[i][t])
+                runs += p_box.linescore[i][t]
+            else:
+                print("x")
+
+            if i % 3 == 0:
+                print(" ")
+
+        if (i - 1) % 3 != 0:
+            print(" ")
+
+        print("-- %2d\n" % runs)
+
+    if p_box.outs_at_end != 3:
+        if not p_box.walk_off:
+            print("  %d out%s when game ended.\n" % (p_box.outs_at_end, "" if p_box.outs_at_end == 1 else "s"))
+        else:
+            print("  %d out%s when winning run was scored.\n" % (p_box.outs_at_end, "" if p_box.outs_at_end == 1 else "s"))
 
 
 # cwbox_print_pitcher_apparatus(boxscore)
@@ -103,7 +153,7 @@ def cwbox_print_apparatus(p_game, p_box, p_vis, p_home):
 
 # void cwbox_print_text(CWGame *game, CWBoxscore *boxscore, CWRoster *visitors, CWRoster *home)
 def cwbox_print_text(p_game, p_box, p_vis, p_home):
-    print( "cwbox_print_text(%s, %s, %s, %s)" % (str(p_game),str(p_box),str(p_vis),str(p_home)) )
+    # print( "cwbox_print_text(%s, %s, %s, %s)" % (str(p_game),str(p_box),str(p_vis),str(p_home)) )
     note_count = 0 # int
     slots = [1, 1] # array int[2]
     players = list() # array CWBoxPlayer[2]
@@ -118,7 +168,7 @@ def cwbox_print_text(p_game, p_box, p_vis, p_home):
 
     cwbox_print_header(p_game, p_vis, p_home)
 
-    print("  %-18s AB  R  H RBI    %-18s AB  R  H RBI\n" %
+    print("  %-18s AB  R  H RBI    %-18s AB  R  H RBI" %
           (p_vis.city if p_vis else cw_game_info_lookup(p_game, "visteam"),
            p_home.city if p_home else cw_game_info_lookup(p_game, "hometeam")) )
 
@@ -144,7 +194,7 @@ def cwbox_print_text(p_game, p_box, p_vis, p_home):
             else:
                 print("%-32s" % ""),
             print(" "),
-        print("\n")
+        print("")
 
     print( "%-20s -- -- -- -- %-22s -- -- -- --\n" % ("","") )
 
@@ -155,23 +205,22 @@ def cwbox_print_text(p_game, p_box, p_vis, p_home):
     print("\n")
 
     cwbox_print_linescore(p_game, p_box, p_vis, p_home)
-
-    print("\n")
+    print("")
 
     for t in range(0, 2):
         pitcher = cw_box_get_starting_pitcher(p_box, t) # CWBoxPitcher
         if t == 0:
-            print("  %-18s   IP  H  R ER BB SO\n" % p_vis.city if p_vis else cw_game_info_lookup(p_game, "visteam"))
+            print("  %-18s   IP  H  R ER BB SO" % p_vis.city if p_vis else cw_game_info_lookup(p_game, "visteam"))
         else:
-            print("  %-18s   IP  H  R ER BB SO\n" % p_home.city if p_home else cw_game_info_lookup(p_game, "hometeam"))
+            print("  %-18s   IP  H  R ER BB SO" % p_home.city if p_home else cw_game_info_lookup(p_game, "hometeam"))
         while pitcher:
             cwbox_print_pitcher(p_game, pitcher, (p_vis if (t == 0) else p_home), note_count)
             pitcher = pitcher.next
         if t == 0:
-            print("\n")
+            print("")
 
     cwbox_print_pitcher_apparatus(p_box)
-    print("\n")
+    print("")
 
     cwbox_print_apparatus(p_game, p_box, p_vis, p_home)
     print("\f")
@@ -189,8 +238,10 @@ try:
             print("found a game.")
             print("type(g1) = %s" % type(g1))
             print("g1 = %s" % g1)
+
             away, at_home = g1.teams
             print("away = %s, home = %s" % (away,at_home))
+
             visitor = cw_roster_create('CAL', 1996, 'AL', 'California', 'Angels')
             with open(cal_1996_roster, "r") as rcfp:
                 cw_roster_read(visitor, rcfp)
@@ -199,14 +250,23 @@ try:
             with open(tor_1996_roster, "r") as rtfp:
                 cw_roster_read(home, rtfp)
             print("type(home) = %s" % type(home))
+
             # cw_game_write(g1, sys.stdout)
+
             print("innings = %s" % g1.innings)
+
+            g1_it = cw_gameiter_create(g1)
+            print("type(g1_it) = %s" % type(g1_it))
+            print("g1_it.totals[1].lob = %s" % g1_it.totals[1].lob)
+
             g1_b = cw_box_create(g1)
             print("type(g1_b) = %s" % type(g1_b))
             # print("boxscore = %s" % g1._get_boxscore())
             cwbox_print_text(g1, g1_b, visitor, home)
+
             for event in g1.events:
                 pass # print("event = %s" % repr(event))
+
             g2 = read_game(gfp)
             print("g2 = %s" % g2)
         else:
